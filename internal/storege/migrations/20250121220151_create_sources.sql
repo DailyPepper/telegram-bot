b3b72195c06c71ceaaddb09a41cff665e7434274
Daniel Kudinov <danielkudinov17@gmail.com>
2025-01-22 12:13:20 +0300

    first commit

---
+++ internal/storege/migrations/20250121220151_create_sources.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE sources (
+    id SERIAL PRIMARY key,
+    name VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    update_at TIMESTAMP NOT NULL DEFAULT NOW(),
+)
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS sources
+-- +goose StatementEnd
